1c736f56b121e4e941d9bc80030e9d8e4428f80a
Abhishek <as400217@gmail.com>
2023-08-18 15:26:09 +0530

    int[] giving errors so instead use List<Integer>

--- DSU/DSU.java
+++ DSU/DSU.java
@@ -3,60 +3,44 @@ package Templates.DSU;
 import java.util.*;
 
 class DisjointSet {
-    int[] parent;
-    int[] rank;
-    int[] size;
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
     public DisjointSet(int n) {
-        // initial config rank->0 , parent -> i
-        parent = rank = size = new int[n + 1];
-        Arrays.fill(rank, 0);
-        Arrays.fill(size, 1);
-
-        for (int i = 0; i <= n; i++)
-            parent[i] = i;
+        for (int i = 0; i <= n; i++) {
+            // i<=n for 1 based indexing
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
     }
 
-    public int findUPar(int u) {
-        // base case
-        if (u == parent[u])
-            return u;
-        /*
-         * finding parent by going till top, then when returning storing it in below
-         * so all below nodes can get parent updated to ultimate parent
-         */
-        int ulp = findUPar(parent[u]);
-        parent[u] = ulp;
-        return ulp;
+    public int findUPar(int node) {
+        // base class
+        if (node == parent.get(node))
+            return node;
+
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
     }
 
     public void unionByRank(int u, int v) {
 
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
-
         if (ulp_u == ulp_v)
             return;
 
-        if (rank[ulp_u] < rank[ulp_v])
-            /*
-             * v
-             * u
-             */
-            parent[ulp_u] = ulp_v;
-        else if (rank[ulp_u] > rank[ulp_v])
-            /*
-             * u
-             * v
-             */
-            parent[ulp_v] = ulp_u;
-        else {
-            /*
-             * u
-             * v
-             */
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
+        if (rank.get(ulp_u) == rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
         }
     }
 
@@ -67,20 +51,10 @@ class DisjointSet {
         if (ulp_u == ulp_v)
             return;
 
-        if (size[ulp_u] < size[ulp_v]) {
-            /*
-             * v
-             * u
-             */
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
         } else {
-            /*
-             * u
-             * v
-             */
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+            parent.set(ulp_v, ulp_u);
         }
     }
 }
